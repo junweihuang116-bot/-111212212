4a4c6ecb3dd4912b31156f4ed5a7c9d70ac88a16
qingning7 <18806119666@163.com>
2025-12-07 16:24:24 +0800

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-        base: '/Babyhan/',
+        base: '/-111212212/',
       server: {
         port: 3000,
         host: '0.0.0.0',
